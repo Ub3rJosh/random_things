545034ae1b9223a82faf0fa86af0a80bdfb673ed
Joshua Maldonado <joshuamaldonado4432@gmail.com>
2024-06-11 23:00:21 -0400

    equations written out and some constants defined in modules.py

---
+++ TOV_solver/modules.py
@@ -0,0 +1,6 @@
+c = 299792458  # m/s
+hbar_joules_per_second = 1.054571817 * 1e-34  # J/s
+hbar_eV_per_second = 6.582119569 * 1e-16  # eV/s
+
+hbarc = 197.326  # MeV fm
+neutron_mass = 939.565  # MeV
