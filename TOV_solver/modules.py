ff6f8935f434544667fa99d32a196004f47a4f02
Joshua Maldonado <joshuamaldonado4432@gmail.com>
2024-06-16 00:43:26 -0400

    first pass at home-grown ode solver use on TOV

--- TOV_solver/modules.py
+++ TOV_solver/modules.py
@@ -29,6 +29,57 @@ def first_order_rk4(x: np.array, y0: float, y0_pr: float, ode_func, *ode_func_ar
         Initial value for derivative
     ode_func : function
         The function that will be solved.
+        Due to the njit decorator, the
+        ode_func must also be numba'd
+        The form should be:
+        d^2/dx^2 y = ode_func(x, y, *ode_func_args)
+    ode_func_args : tuple (optional)
+        Args for ode_func
+    
+    Returns
+    -------
+    y_pr : numpy array
+        The solution to the given ODE as given by
+        rk4.
+    """
+    n = len(x)
+    dx = x[1] - x[0]  # THIS ASSUMES A LINEAR MESH !!
+    half_dx = dx / 2
+    # initialize return array & set initial value(s)
+    y = np.empty(n)
+    y[0] = y0
+    y[1] = y0_pr
+    
+    for i in np.arange(1, n-1):
+        F_1 = dx * ode_func(x[i], y[i], *ode_func_args)
+        F_2 = dx * ode_func(x[i] + half_dx, y[i] + F_1 / 2, *ode_func_args)
+        F_3 = dx * ode_func(x[i] + half_dx, F_2 / 2, *ode_func_args)
+        F_4 = dx * ode_func(x[i+1], y[i] + F_3, *ode_func_args)
+        
+        y[i + 1] = ode_func(x[i], y[i], *ode_func_args) + ((1 / 6) * (F_1 + 2 * F_2 + 3 * F_3 + F_4))
+    
+    return y
+
+
+def first_order_rk4_slow(x: np.array, y0: float, y0_pr: float, ode_func, *ode_func_args):
+    """
+    This version does _not_ use numba.
+    This is convenient because it can be used with
+    a generic python function
+    rk4 solver using the equations that I
+    shamelessly ripped from:
+    https://www.intmath.com/differential-equations/12-runge-kutta-rk4-des.php
+    
+    Parameters
+    ----------
+    x : numpy array
+        Mesh for solver
+    y0 : number
+        Initial point for y
+    y0_pr : number
+        Initial value for derivative
+    ode_func : function
+        The function that will be solved.
         The form should be:
         d^2/dx^2 y = ode_func(x, y, *ode_func_args)
     ode_func_args : tuple (optional)
